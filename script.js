b9bb7abd6db8909ed1d2791025db165cd85fa74f
Victorerichschmidt <159267724+Victorerichschmidt@users.noreply.github.com>
2024-09-03 10:44:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 function calcula(opera){
-    let num1 = parseFloat(documento.getElementById('zeroum').value);
-    let num1 = parseFloat(documento.getElementById('zerodois').value);
-    let resultado = 0
+    let num1 = parseFloat(document.getElementById('zeroum').value);
+    let num2 = parseFloat(document.getElementById('zerodois').value);
+    let resultado = 0;
     if(isNaN(num1) || isNaN(num2)){
-        alert('acorde para a vida.');
+        alert('ACORDE PARA A VIDA.');
     }else{
-        if (opera == "+") {
+        if(opera == '+') {
             resultado = num1 + num2;
-        } else if (opera == "-") {
+        } else if(opera == '-') {
             resultado = num1 - num2;
-        } else if (opera == "*") {
+        } else if(opera == '*') {
             resultado = num1 * num2;
-        } else if (opera == "/") {
+        } else if(opera == '/') {
             resultado = num1 / num2;
         }
+        document.getElementById('resultado').textContent = 'resultado: ' + resultado;
     }
 }
